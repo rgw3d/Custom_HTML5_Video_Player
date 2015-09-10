d61a616721344c8266eb72d9f445b7529ef6f634
rgw3d <rgw3d@aol.com>
2015-09-09 23:13:18 -0400

    Working video pause/play with separate button

--- assets/js/video-player.js
+++ assets/js/video-player.js
@@ -3,8 +3,34 @@ $(function() {
 });
 
 var mediaPlayer;
+var mediaPlayerDOM;
 function initMediaPlayer() {
-    mediaPlayer = $('#video');
-    mediaPlayer.attr('controls', false);
+    $('#video').attr('controls', false);
+    mediaPlayerDOM = $('#video').get(0);
 
+    
 }
+
+$('#play-toggle').click(function() {
+        if(mediaPlayerDOM.paused){
+            mediaPlayerDOM.play();
+            this.title = 'pause';
+            this.innerHTML = 'pause';
+            this.className = 'pause';
+        }
+        else{
+            mediaPlayerDOM.pause();
+            this.title = 'play';
+            this.innerHTML = 'play';
+            this.className = 'play';
+        }
+    });
+
+function togglePlayPause(){
+    var button = $('play-toggle');
+    if(mediaPlayer.paused)
+        alert('is paused');
+
+
+
+}    
